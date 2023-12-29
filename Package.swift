a89229f2ebb79f26ffc0751ffcbce7253ed94e7e
Ilgiz Fazlyev <fazliev.ilgiz@yandex.ru>
2023-12-29 19:55:01 +0500

    up man

--- Package.swift
+++ Package.swift
@@ -3,17 +3,11 @@ import PackageDescription
 
 let package = Package(
     name: "libyuv",
-    platforms: [
-        .iOS(.v11)
-    ],
     products: [
-        .library(name: "libyuv",
-                 targets: ["libyuv"]),
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(name: "libyuv", targets: ["libyuv"])
     ],
     targets: [
-        .binaryTarget(
-            name: "libyuv",
-            path: "Sources/libyuv.xcframework"
-        )
+        .binaryTarget(name: "libyuv", path: "Sources/libyuv.xcframework")
     ]
 )
